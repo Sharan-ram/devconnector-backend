df47b64faf2145289b406b97488265c34d1ca3c8
Sharan-ram <sharanram1993@gmail.com>
2020-03-28 01:14:44 +0530

    Create Posts Schema

---
+++ models/Post.js
@@ -0,0 +1,53 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "user"
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  avatar: {
+    type: String
+  },
+  text: {
+    type: String,
+    required: true
+  },
+  likes: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: "user"
+      }
+    }
+  ],
+  comments: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: "user"
+      },
+      date: {
+        type: Date,
+        default: Date.now
+      },
+      name: {
+        type: String,
+        required: true
+      },
+      avatar: {
+        type: String
+      }
+    }
+  ],
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = Post = mongoose.model("post", PostSchema);
