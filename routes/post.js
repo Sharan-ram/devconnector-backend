9594bff7ffb0f2fe0105d5874f8fda9aba16dbe8
Sharan-ram <sharanram1993@gmail.com>
2020-03-28 02:27:07 +0530

    Add an API to get post by postId

--- routes/post.js
+++ routes/post.js
@@ -38,6 +38,25 @@ router.get("/", authMiddleware, async (req, res) => {
   }
 });
 
+// @Route:   GET /:id
+// @desc:    Get post by post id
+// @Access:  Private
+router.get("/:id", authMiddleware, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) {
+      return res.status(400).json({ errors: [{ msg: "Post not found" }] });
+    }
+    res.json(post);
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind == "ObjectId") {
+      return res.status(400).json({ errors: [{ msg: "Post not found" }] });
+    }
+    res.status(500).send("Internal Server Error");
+  }
+});
+
 // @Route:   POST /
 // @desc:    Create a new post
 // @Access:  Private
