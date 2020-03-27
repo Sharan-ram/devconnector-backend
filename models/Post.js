376318f2d2eeb05fe255f8abb125649de4a4bda0
Sharan-ram <sharanram1993@gmail.com>
2020-03-28 03:36:07 +0530

    Add text field to the Post Schema

--- models/Post.js
+++ models/Post.js
@@ -27,6 +27,10 @@ const PostSchema = new Schema({
   ],
   comments: [
     {
+      text: {
+        type: String,
+        required: true
+      },
       user: {
         type: Schema.Types.ObjectId,
         ref: "user"
