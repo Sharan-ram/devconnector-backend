160fc65fc0a768cf01254abf852ae14f749de38f
Sharan-ram <sharanram1993@gmail.com>
2020-03-28 18:43:42 +0530

    Change port

--- server.js
+++ server.js
@@ -28,6 +28,6 @@ app.use("/api/profile", profileRoutes);
 // Posts routes
 app.use("/api/posts", postRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
