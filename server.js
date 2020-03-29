546f2780929db0fabda7626b85c79a4251edd62c
Sharan-ram <sharanram1993@gmail.com>
2020-03-30 00:41:54 +0530

    Enable CORS

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const dbConnect = require("./config/db");
 
 const userRoutes = require("./routes/users");
@@ -10,6 +11,7 @@ const postRoutes = require("./routes/post");
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 // Connect to MongoDB
 dbConnect();
