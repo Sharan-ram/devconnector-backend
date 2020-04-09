9811ea7aa83245770ef15b7d91fcead414a45124
Sharan-ram <sharanram1993@gmail.com>
2020-04-09 12:09:19 +0530

    Add API endpoint to add a new experience

--- routes/profile.js
+++ routes/profile.js
@@ -163,6 +163,41 @@ router.post(
   }
 );
 
+//@Route   : POST /experience
+//@Desc    : Add new experience
+//@Access  : Private
+router.post(
+  "/experience",
+  [
+    authMiddleware,
+    check("title", "Title is required").not().isEmpty(),
+    check("company", "Company is required").not().isEmpty(),
+    check("from", "Start date is required").not().isEmpty(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    try {
+      const profile = await Profile.findOne({ user: req.user.id });
+      if (!profile) {
+        return res.status(400).json({
+          errors: [
+            { msg: "Please update the profile before adding experience" },
+          ],
+        });
+      }
+      profile.experience.push(req.body);
+      await profile.save();
+      res.json(profile);
+    } catch (err) {
+      console.error(err);
+      res.status(500).send("Internal Server Error");
+    }
+  }
+);
+
 // @Route  : DELETE /
 // @Desc   : Delete user and profile of the logged in user
 // @Access : Private
